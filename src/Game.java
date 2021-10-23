7883ac42ff880ed4508d7bf889acb07d545fb9e4
AishaSaleem110 <aishasalim.cs72@gmail.com>
2021-10-23 11:43:57 +0100

    printLocationInfo method created
    Duplicated code removed from Game class by creating a more cohesive method.

--- src/Game.java
+++ src/Game.java
@@ -87,27 +87,7 @@ public class Game {
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
         System.out.println("Type 'help' if you need help.");
         System.out.println();
-        System.out.println("You are " + currentRoom.getDescription());
-        System.out.print("Exits: ");
-        if (currentRoom.northExit != null) {
-            System.out.print("north ");
-        }
-        if (currentRoom.eastExit != null) {
-            System.out.print("east ");
-        }
-        if (currentRoom.southExit != null) {
-            System.out.print("south ");
-        }
-        if (currentRoom.westExit != null) {
-            System.out.print("west ");
-        }
-        System.out.println();
-        System.out.print("Items: ");
-        if (currentRoom.itemDescription != null) {
-            System.out.print(currentRoom.itemDescription
-                    + '(' + currentRoom.itemWeight + ')');
-        }
-        System.out.println();
+        printLocationInfo();
     }
 
     /**
@@ -188,27 +168,7 @@ public class Game {
             System.out.println("There is no door!");
         } else {
             currentRoom = nextRoom;
-            System.out.println("You are " + currentRoom.getDescription());
-            System.out.print("Exits: ");
-            if (currentRoom.northExit != null) {
-                System.out.print("north ");
-            }
-            if (currentRoom.eastExit != null) {
-                System.out.print("east ");
-            }
-            if (currentRoom.southExit != null) {
-                System.out.print("south ");
-            }
-            if (currentRoom.westExit != null) {
-                System.out.print("west ");
-            }
-            System.out.println();
-            System.out.print("Items: ");
-            if (currentRoom.itemDescription != null) {
-                System.out.print(currentRoom.itemDescription
-                        + '(' + currentRoom.itemWeight + ')');
-            }
-            System.out.println();
+           printLocationInfo();
         }
     }
 
@@ -216,27 +176,7 @@ public class Game {
      * "Look" was entered. Report what the player can see in the room
      */
     private void look() {
-        System.out.println("You are " + currentRoom.getDescription());
-        System.out.print("Exits: ");
-        if (currentRoom.northExit != null) {
-            System.out.print("north ");
-        }
-        if (currentRoom.eastExit != null) {
-            System.out.print("east ");
-        }
-        if (currentRoom.southExit != null) {
-            System.out.print("south ");
-        }
-        if (currentRoom.westExit != null) {
-            System.out.print("west ");
-        }
-        System.out.println();
-        System.out.print("Items: ");
-        if (currentRoom.itemDescription != null) {
-            System.out.print(currentRoom.itemDescription
-                    + '(' + currentRoom.itemWeight + ')');
-        }
-        System.out.println();
+        printLocationInfo();
     }
 
     /**
@@ -337,5 +277,28 @@ public class Game {
         } else {
             return true;  // signal that we want to quit
         }
+    }
+    private void printLocationInfo(){
+        System.out.println("You are " + currentRoom.getDescription());
+        System.out.print("Exits: ");
+        if (currentRoom.northExit != null) {
+            System.out.print("north ");
+        }
+        if (currentRoom.eastExit != null) {
+            System.out.print("east ");
+        }
+        if (currentRoom.southExit != null) {
+            System.out.print("south ");
+        }
+        if (currentRoom.westExit != null) {
+            System.out.print("west ");
+        }
+        System.out.println();
+        System.out.print("Items: ");
+        if (currentRoom.itemDescription != null) {
+            System.out.print(currentRoom.itemDescription
+                    + '(' + currentRoom.itemWeight + ')');
+        }
+        System.out.println();
     }
 }
